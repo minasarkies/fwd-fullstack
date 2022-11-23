aaf346ad5b954d1e3b3bce899f31ba2775264688
minasarkies <65076021+minasarkies@users.noreply.github.com>
2022-11-23 18:11:13 +0200

    Update exists.ts

--- src/routers/exists.ts
+++ src/routers/exists.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 const router1 = express.Router();                         // Create express router object
 
 router1.get('/',  async (req: express.Request, res: express.Response) => {
-      fs.readdir('./src/images', (err, files) => {       // Creating the array that will house all the images so the 
+      fs.readdir('./src/images/full', (err, files) => {       // Creating the array that will house all the images so the 
                                                          // ones that have been processed do not have to take up resouces again.
       const img_arr = files.map((file) => file.split('.')[0]);
       res.send(JSON.stringify(img_arr));
